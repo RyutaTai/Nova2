be5901c75a1d8246055095918ed23a028afe9d3e
MSI\2220412 <z.ryu.d.0822@gmail.com>
2025-01-17 09:21:20 +0900

    ボスの当たり判定途中。 Spriteの色と透明度を変えられるようになった。 SpriteとかModelにポジションとか色とか持たせるのはよくないから、時間あるときに直す。

--- Source/Nova/Shaders/SpritePs.hlsl
+++ Source/Nova/Shaders/SpritePs.hlsl
@@ -15,7 +15,7 @@ float4 main(VS_OUT pin) : SV_TARGET
     const float GAMMA = 1/2.2;
     color.rgb = pow(color.rgb, GAMMA);
 #endif
-    return color;
+    //return color;
     return float4(color.rgb, alpha) * pin.color;
     
     //return color_map.Sample(point_sampler_state, pin.texcoord) * pin.color; //  Unit8
